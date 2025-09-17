81c0d2056f875d78198fa902381ab532c35486f5
y.c.choi <y.c.choi@sbs.co.kr>
2025-09-17 15:59:45 +0900

    Remove submodule and add as normal directory

---
+++ SPRING/20250917_book_springboot-developer-2rd/springboot-developer/src/main/java/study/springbootdeveloper/springbootdeveloper/Member.java
@@ -0,0 +1,27 @@
+package study.springbootdeveloper.springbootdeveloper;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ SPRING/20250917_book_springboot-developer-2rd/springboot-developer/src/main/java/study/springbootdeveloper/springbootdeveloper/MemberRepository.java
@@ -0,0 +1,12 @@
+package study.springbootdeveloper.springbootdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findAll();
+}
